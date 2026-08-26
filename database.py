394055abcefc23e395caad5569909f24ae7469b9
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-08-26 11:49:44 +0100

    Add SELECT and INSERT operations

--- database.py
+++ database.py
@@ -1,9 +1,21 @@
 import sqlite3 # Use the functionality provided by sqlite3 module
 
+def get_observations():
+    cursor.execute("SELECT * FROM observations")
+    return cursor.fetchall()
+
+
+def add_observation(observation):
+    cursor.execute("""INSERT INTO observations (species, confidence, date, location, image)
+    VALUES (?, ?, ?, ?, ?)""", (observation["species"], observation["confidence"], observation["date"], observation["location"],
+                                observation["image"]))
+    # ? are parameterised placeholders
+    connection.commit() # commits the newly inserted row to the SQLite database file so that it persists on the disk
+
 connection = sqlite3.connect("fauna.db")
 cursor = connection.cursor() # Cursor is like a messenger
 cursor.execute("""CREATE TABLE IF NOT EXISTS observations (
-    obs_id INTEGER PRIMAY KEY,
+    obs_id INTEGER PRIMARY KEY,
     species TEXT,
     confidence REAL,
     date TEXT,
@@ -11,5 +23,19 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS observations (
     image TEXT
 );""") # if not exists part ensures code does not crash if we already have fauna.db
 connection.commit()
-connection.close()
 
+
+
+test_observation = {
+    "species": "Red Fox",
+    "confidence": 0.91,
+    "date": "17/08/26",
+    "location": "Clapham",
+    "image" : "fox.jpg"
+}
+
+add_observation(test_observation)
+results = get_observations()
+print(results)
+
+connection.close()
