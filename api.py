81f7dd7d67b7169c2d368b7b62be241cf50cf6b9
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-08-28 11:22:41 +0100

    Setting up pydantic

--- api.py
+++ api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from database import get_observations
+from pydantic import BaseModel
+from database import add_observation
 
 app = FastAPI()
+
+class Observation(BaseModel):
+    species: str
+    confidence: float
+    date: str
+    location: str
+    image: str
 
 @app.get("/")
 def root():
@@ -10,3 +19,9 @@ def root():
 @app.get("/observations")
 def get_all_observations():
     return get_observations()
+
+@app.post("/observations")
+def create_observation(observation: Observation):
+    add_observation(observation.model_dump())
+
+
